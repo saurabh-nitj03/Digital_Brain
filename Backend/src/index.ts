812c86eb8d9f1db556a25cdf9e489b3b13439632
saurabh-nitj03 <sauravkrishnachauhan@gmail.com>
2025-06-22 20:01:30 +0530

    Frontend connection

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://digital-brain-cgo2.onrender.com"],
     credentials: true,
   })
 );
