f82c8298423eb1d54954e967e352fa46610e60be
saurabh-nitj03 <sauravkrishnachauhan@gmail.com>
2025-06-23 00:43:46 +0530

    Handled build

--- Backend/dist/controllers/user.controller.js
+++ Backend/dist/controllers/user.controller.js
@@ -91,7 +91,7 @@ const signin = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
             httpOnly: true,
             secure: true,
             maxAge: 24 * 60 * 60 * 1000,
-            sameSite: "None"
+            sameSite: 'none'
         });
         return res.status(200).json({
             success: true,
--- Backend/src/controllers/user.controller.ts
+++ Backend/src/controllers/user.controller.ts
@@ -79,7 +79,7 @@ export const signin=async(req:any,res:any)=>{
             httpOnly: true,
             secure: true,
             maxAge: 24 * 60 * 60 * 1000 ,
-            sameSite: "None"
+            sameSite: 'none'
         });
         return res.status(200).json({
                 success:true,
